acd1126e18d768c9a575a95e0f45e86ea80b9dde
Qomojay <1056638377@qq.com>
2020-02-28 15:34:57 +0800

    2.27

---
+++ 287.cpp
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: 287.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月28日 星期五 15时33分19秒
+ ************************************************************************/
+
+//寻找重复数
+
+int findDuplicate(int* nums, int numsSize){
+    int p = nums[0], q = nums[0];
+    do {
+        p = nums[p];
+        q = nums[nums[q]];
+    }while (p != q);
+    q = nums[0];
+    while (p != q) {
+        p = nums[p];
+        q = nums[q];
+    }
+    return p;
+
+    
+}
